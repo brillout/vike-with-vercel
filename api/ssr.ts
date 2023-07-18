ff1f8896f0e220ec613951293d4ae29674457a34
Romuald Brillout <git@brillout.com>
2023-07-18 12:59:04 +0200

    use TypeScript for API route

--- api/ssr.ts
+++ api/ssr.ts
@@ -1,7 +1,9 @@
 import { renderPage } from 'vite-plugin-ssr'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default async function handler(req, res) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { url } = req
+  if (url === undefined) throw new Error('req.url is undefined')
   console.log('Request to url:', url)
 
   const pageContextInit = { url }
--- pages/about/index.page.vue
+++ pages/about/index.page.vue
@@ -1,5 +1,5 @@
 <template>
   <h1>About</h1>
   <p>Showcase of using vite-plugin-ssr with Vercel.</p>
-  <p>Example version: 2.</p>
+  <p>Example version: 3.</p>
 </template>
