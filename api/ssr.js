312af6436f96761d52dcc9272cebca7c4763522b
Romuald Brillout <git@brillout.com>
2023-07-18 13:31:59 +0200

    don't use TypeScript

--- api/ssr.js
+++ api/ssr.js
@@ -1,7 +1,6 @@
 import { renderPage } from 'vite-plugin-ssr'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   const { url } = req
   console.log('Request to url:', url)
   if (url === undefined) throw new Error('req.url is undefined')
--- pages/about/index.page.vue
+++ pages/about/index.page.vue
@@ -1,5 +1,5 @@
 <template>
   <h1>About</h1>
   <p>Showcase of using vite-plugin-ssr with Vercel.</p>
-  <p>Example version: 3.</p>
+  <p>Example version: 4.</p>
 </template>
